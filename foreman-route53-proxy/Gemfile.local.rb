1330ef701e669cc142a88d831ca02b52e06535a5
Ashley Penney <apenney@ntoggle.com>
2016-04-13 01:01:15 +0000

    Catch up with the huge number of changes made to make this work.

---
+++ foreman-route53-proxy/Gemfile.local.rb
@@ -0,0 +1,2 @@
+gem 'smart_proxy_dns_route53', :git => "https://github.com/theforeman/smart_proxy_dns_route53.git"
+gem 'route53'
--- foreman-tftpd-proxy/setup_tftpd.sh
+++ foreman-tftpd-proxy/setup_tftpd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i "s/REPLACE_HOST/${TFTPD_HOST:?ERROR: TFTPD_HOST env var not set}/g" /usr/share/smart-proxy/config/tftp.yml
+sed -i "s/REPLACE_HOST/${TFTPD_HOST:?ERROR: TFTPD_HOST env var not set}/g" /usr/share/smart-proxy/config/settings.d/tftp.yml
